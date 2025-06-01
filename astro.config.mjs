de08d92813ffdd6ec5e915a702dcccc94838175a
fanioz <1700770+fanioz@users.noreply.github.com>
2025-06-01 11:30:18 +0700

    Setup GitHub Pages untuk AstroJS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	output: 'static',
+  base: '/monesia.github.io/', // Sesuaikan dengan nama repository Anda
+	//site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
