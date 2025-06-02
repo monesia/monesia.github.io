bf57aebf2c64b5b76583419ef41f3ef356eee427
fanioz <1700770+fanioz@users.noreply.github.com>
2025-06-02 11:41:44 +0700

    Update config for local deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,20 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+// Tentukan apakah kita sedang dalam mode build untuk GitHub Pages atau development
+// Anda bisa menggunakan variabel lingkungan lain jika lebih sesuai dengan alur kerja Anda
+const IS_GITHUB_PAGES_BUILD = process.env.NODE_ENV === 'production'; // Contoh sederhana, bisa disesuaikan
+
 export default defineConfig({
-	output: 'static',
-  base: '/monesia.github.io/', // Sesuaikan dengan nama repository Anda
-	//site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  output: 'static',
+
+  // (PENTING) Setel URL utama situs Anda di sini
+  // Ganti '<USERNAME>' dengan username GitHub Anda.
+  site: 'https://monesia.github.io',
+
+  // (PENTING) Atur 'base' hanya untuk build produksi ke GitHub Pages subdirectory
+  // Saat development, 'base' akan menjadi '/'
+  base: IS_GITHUB_PAGES_BUILD ? '/monesia.github.io/' : '/',
+
+  integrations: [mdx(), sitemap()],
 });
