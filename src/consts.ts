d410c5e32ee4ab92255487813b219add2b7d1915
fanioz <1700770+fanioz@users.noreply.github.com>
2025-06-02 13:57:26 +0700

    Update title, description and header menu

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Monesia: Analisa Saham & Investasi Cerdas';
+export const SITE_DESCRIPTION = 'Blog Monesia membahas analisa saham, laporan keuangan, valuasi perusahaan, dan tips praktis investasi untuk mencapai kebebasan finansial.';
