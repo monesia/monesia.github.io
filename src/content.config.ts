c0b6c8c34e40b0401c63cd6bf9d79604d6f14b19
fanioz <1700770+fanioz@users.noreply.github.com>
2025-06-05 10:55:59 +0700

    Update blog design and layout

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,16 @@
-import { glob } from 'astro/loaders';
+// src/content.config.ts
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+    // Type-check frontmatter using a schema
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        // Transform string to Date object
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+        heroImage: z.string().optional(),
+    }),
 });
 
 export const collections = { blog };
